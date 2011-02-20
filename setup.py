aa373b0b178a2b8deba8dd4d3818cdca9e40ddad
Sean O'Connor <sean@seanoc.com>
2011-02-20 16:43:38 -0500

    Minor cleanup.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='Humus',
     version='0.1.0',
-    packages=find_packages(),#['humus',],
+    packages=find_packages(),
     entry_points = {
         'console_scripts': [
             'humus = humus.commands:humus_sync',
@@ -11,4 +11,17 @@ setup(
     },
     license='MIT',
     long_description=open('README.rst').read(),
+    install_requires=['boto>=2.0b4'],
+    author = "Sean O'Connor",
+    author_email = "sean@saaspire.com",
+    description = "A simple S3 backup script",
+    keywords = "amazon aws s3 backup",
+    url = "https://github.com/SeanOC/humus",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: System :: Archiving :: Backup',
+    ],
 )
