04b478616e50a3b264b094c0c3b0f065836018f0
Sean O'Connor <sean@seanoc.com>
2011-02-20 14:31:53 -0500

    Added trim functionality.

--- humus/core.py
+++ humus/core.py
@@ -1,11 +1,15 @@
 from bz2 import BZ2File
 import ConfigParser
+from datetime import datetime
+import logging
 import os
 import tempfile
 
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 from humus.exceptions import ConfigError
+
+logger = logging.getLogger('humus.Syncer')
 
 class Syncer(object):
 
@@ -16,6 +20,13 @@ class Syncer(object):
         '/etc/humus/humus.ini',
     ]
 
+    @classmethod
+    def now(cls):
+        '''
+        Wrapper for datetime.utcnow which allows the called to be mocked in testing
+        '''
+        return datetime.utcnow()
+
     def __init__(self, config_paths=None):
         if config_paths is not None:
             self.config_paths = config_paths
@@ -25,7 +36,16 @@ class Syncer(object):
 
         self.working_dir = tempfile.mkdtemp()
         self.to_cleanup = [self.working_dir]
-        self.chunk_size = 1024
+        self.path = self.config.get('AWS', 'path', '')
+        if self.config.has_section('humus'):
+            self.chunk_size = self.config.getint('humus', 'chunk_size', 1024)
+            self.count_limit = self.config.getint('humus', 'count_limit', None)
+            self.age_limit = self.config.getint('humus', 'age_limit', None)
+        else:
+            self.chunk_size = 1024
+            self.count_limit = None
+            self.age_limit = None
+
         super(Syncer, self).__init__()
 
     def get_bucket(self):
@@ -52,21 +72,41 @@ class Syncer(object):
 
     def sync(self, source, target_name):
         compressed_path = self.compress_data(source)
-        target_name += '.bz2'
+        now = self.now()
+        now_str = now.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+        target_name += '.%s.bz2' % now_str
         bucket = self.get_bucket()
         key = Key(bucket)
-        key.key = os.path.join(self.config.get('AWS','path', ''), target_name)
+        key.key = os.path.join(self.path, target_name)
+        logger.info('Uploading to %s' % key.key)
+        key.set_metadata('created', now_str)
         key.set_contents_from_filename(compressed_path)
         key.set_acl('private')
+
+    def trim(self):
+        bucket = self.get_bucket()
+        keys = bucket.list(prefix=self.path)
+
+        for n, key in enumerate(keys, start=1):
+
+            if self.count_limit and self.count_limit > n:
+                key.delete()
+
+            if self.age_limit:
+                key.open_read()
+                now = self.now()
+                created_str = key.get_metadata('created')
+                if created_str:
+                    created = datetime.strptime(created_str, '%Y-%m-%dT%H:%M:%S.000Z')
+                    age = now - created
+                    if self.age_limit <= age.days:
+                        key.delete()
 
     def cleanup(self):
         for directory in self.to_cleanup:
             os.removedirs(directory)
 
         self.to_cleanup = []
-
-    def rotate(self):
-        keys = 1
 
 
 
--- tests/core_tests.py
+++ tests/core_tests.py
@@ -1,10 +1,13 @@
 import ConfigParser
 from copy import copy
+from datetime import datetime, timedelta
 import os
+import time
 from unittest import TestCase
 import bz2
 
 from boto.s3.connection import S3Connection
+from mock import patch
 from nose.tools import raises
 from testconfig import config as test_config
 
@@ -81,7 +84,74 @@ class CoreTests(TestCase):
             expected = bz2.compress(test_file.read())
 
         bucket = self.get_bucket()
-        key = bucket.get_key('test/text_file.txt.bz2')
+        keys = list(bucket.list())
+        key = keys[0]
         result = key.get_contents_as_string()
 
         self.assertEquals(expected, result)
+
+    def build_file_history(self, syncer, count, utcnow_mock):
+        test_file_path = os.path.join(self.test_files_dir, 'text_file.txt')
+        with open(test_file_path) as test_file:
+            offsets = range(count)
+            offsets.reverse()
+            for i in offsets:
+                test_file.seek(0)
+                utcnow_mock.return_value = datetime.utcnow() - timedelta(days=i, minutes=1)
+                syncer.sync(test_file, 'text_file.txt')
+
+
+
+    @patch.object(Syncer, 'now')
+    def test_trim_by_count(self, utcnow_mock):
+        # Setup everything needed for the test
+        syncer = self.get_syncer()
+        syncer.count_limit = 2
+        self.build_file_history(syncer, count=3, utcnow_mock=utcnow_mock)
+
+        # Get a bucket to work with
+        bucket = self.get_bucket()
+
+        # Check that the pre trim count is correct
+        expected = 3
+        result = len(list(bucket.list()))
+        self.assertEquals(expected, result)
+
+        # Trim the files
+        syncer.trim()
+
+        # Check that the post trime count is correct
+        expected = 2
+        result = len(list(bucket.list()))
+        self.assertEquals(expected, result)
+
+    @patch.object(Syncer, 'now')
+    def test_trim_by_age(self, utcnow_mock):
+        # Setup everything needed for the test
+        syncer = self.get_syncer()
+        syncer.age_limit = 1
+        self.build_file_history(syncer, count=3, utcnow_mock=utcnow_mock)
+
+        # Get a bucket to work with
+        bucket = self.get_bucket()
+
+
+        # Check that the pre trim count is correct
+        expected = 3
+        result = len(list(bucket.list()))
+        self.assertEquals(expected, result)
+
+        # Trim the files
+        utcnow_mock.return_value = datetime.utcnow()
+        syncer.trim()
+
+
+        # Check that the pre trim count is correct
+        expected = 1
+        keys = list(bucket.list())
+        for key in keys:
+            key.open_read()
+
+        result = len(keys)
+        self.assertEquals(expected, result,
+        "Too many keys returned.  Created dates for returned keys:  %s" % [key.open_read(); key.get_metadata('created') for key in keys])
