a2ab4c139c4e90b24bf82bc4a6e8366a56936c33
Sean O'Connor <sean@seanoc.com>
2011-02-20 11:00:27 -0500

    Added basic sync.

--- humus/core.py
+++ humus/core.py
@@ -1,13 +1,17 @@
+from bz2 import BZ2File
 import ConfigParser
 import os
+import tempfile
 
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
 from humus.exceptions import ConfigError
 
 class Syncer(object):
 
     config_paths = [
         './humus.ini',
-            '~/humus.ini',
+        '~/humus.ini',
         '/etc/humus.ini',
         '/etc/humus/humus.ini',
     ]
@@ -15,11 +19,49 @@ class Syncer(object):
     def __init__(self, config_paths=None):
         if config_paths is not None:
             self.config_paths = config_paths
-        super(Syncer, self).__init__()
 
+        self.config = self.load_config()
+        self.conn = S3Connection(self.config.get('AWS', 'access_key'), self.config.get('AWS', 'secret_key'))
+
+        self.working_dir = tempfile.mkdtemp()
+        self.to_cleanup = [self.working_dir]
+        self.chunk_size = 1024
+        super(Syncer, self).__init__()
 
     def load_config(self):
         config = ConfigParser.SafeConfigParser()
         config.read([os.path.abspath(path) for path in self.config_paths])
 
         return config
+
+    def compress_data(self, source):
+        tmp_file_path = os.path.join(self.working_dir, 'compessed_file.bz2')
+        target = BZ2File(tmp_file_path, mode='w')
+
+        chunk = source.read(self.chunk_size)
+        while chunk != '':
+            target.write(chunk)
+            chunk = source.read(self.chunk_size)
+
+        target.close()
+
+        return tmp_file_path
+
+    def sync(self, source, target_bucket, target_name, target_path=''):
+        compressed_path = self.compress_data(source)
+        target_name += '.bz2'
+        bucket = self.conn.create_bucket(target_bucket)
+        key = Key(bucket)
+        key.key = os.path.join(target_path, target_name)
+        key.set_contents_from_filename(compressed_path)
+        key.set_acl('private')
+
+    def cleanup(self):
+        for directory in self.to_cleanup:
+            os.removedirs(directory)
+
+        self.to_cleanup = []
+
+
+
+
--- tests/core_tests.py
+++ tests/core_tests.py
@@ -1,8 +1,10 @@
 import ConfigParser
+from copy import copy
 import os
 from unittest import TestCase
+import bz2
 
-from mock import Mock, patch
+from boto.s3.connection import S3Connection
 from nose.tools import raises
 from testconfig import config as test_config
 
@@ -32,18 +34,52 @@ class CoreTests(TestCase):
     def tearDownClass(cls):
         os.remove(cls.test_humus_config)
 
-    def get_syncer(self):
+    def tearDown(self):
+        '''
+        Cleanup anything that may have been put on S3.
+        '''
+        bucket = self.get_bucket()
+        for key in bucket.list():
+            key.delete()
+        bucket.delete()
 
+    def get_bucket(self):
+        conn = S3Connection(test_config['AWS']['access_key'], test_config['AWS']['secret_key'])
+        bucket = conn.create_bucket(test_config['AWS']['bucket'])
+
+        return bucket
+
+    def get_syncer(self):
         syncer = Syncer(config_paths=[self.test_humus_config,])
         return syncer
 
     def test_init(self):
         syncer = self.get_syncer()
+        self.assertEquals(test_config['AWS']['access_key'], syncer.config.get('AWS', 'access_key'))
+        self.assertEquals(test_config['AWS']['secret_key'], syncer.config.get('AWS', 'secret_key'))
 
+    def test_cleanup(self):
+        syncer = self.get_syncer()
+        to_cleanup = copy(syncer.to_cleanup)
 
-    @patch.object(ConfigParser, 'SafeConfigParser')
-    def test_load_config(self, parser_mock):
-        syncer = Syncer()
-        config = syncer.load_config()
+        for directory in to_cleanup:
+            self.assertTrue(os.path.exists(directory))
 
-        assert parser_mock.called
+        syncer.cleanup()
+
+        for directory in to_cleanup:
+            self.assertFalse(os.path.exists(directory))
+
+    def test_sync(self):
+        syncer = self.get_syncer()
+        test_file_path = os.path.join(self.test_files_dir, 'text_file.txt')
+        with open(test_file_path) as test_file:
+            syncer.sync(test_file, test_config['AWS']['bucket'], 'text_file.txt')
+            test_file.seek(0)
+            expected = bz2.compress(test_file.read())
+
+        bucket = self.get_bucket()
+        key = bucket.get_key('text_file.txt.bz2')
+        result = key.get_contents_as_string()
+
+        self.assertEquals(expected, result)
