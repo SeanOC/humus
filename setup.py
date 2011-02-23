8483439821c69d5680c2bada2eb1ad9a9ac26588
Sean O'Connor <sean@seanoc.com>
2011-02-23 11:51:27 -0500

    Version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Humus',
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(),
     entry_points = {
         'console_scripts': [
